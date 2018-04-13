1bf275f7815c98ae83e3cfaf26b32f8e7b3c98b1
Zac Keilholz <keilh001@umn.edu>
2018-04-12 21:16:51 -0500

    Initial HTML created, JS question library populating on screen, player choice is being checked if right or wrong.  Next Steps: Create a YEP or NOPE dialogue box, then incoporate timer element.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,11 +36,106 @@
         $("#movies-view").append("<button data-name='moviename '>"+movies[i]+"</button>");
         */
 
-    
-
-
-    
-
-
-
+/* 
+Awesome JS Snippets- 
+cl - console log
+si - set interval
+st - set timeout
+jqclass - jq class
+jqempty
+jqdoc - document ready
 */
+
+$(document).ready(function () {
+
+    //Define Epic Object
+
+    var triviaGame = {
+        placeholder: "something",
+
+
+        gameStart: false,
+        questionNumber: 0,
+        questionLibrary:
+            [{
+                question: "Fav Fruit?",
+                answer: 2,
+                choices: ["apple", "bananaIsRight", "orange"],
+            }, {
+                question: "Yo Mama?",
+                answer: 0,
+                choices: ["apple", "bananaIsRight", "orange"],
+            }],
+
+        //Empty Main Content Div and Grab/Append Questions in HTML
+
+        printQuizToScreen: function () {
+            console.log("PrintQtoScreen");
+            console.log(this.questionNumber);
+            //If this.questionnumber=questions.length - game end
+            // else timer enabled
+
+
+            //Empty Current Div Contents
+            var gameContainerTarget = $("#game-container");
+            gameContainerTarget.empty();
+
+            //Get object Target
+
+            //Retrieve question from object
+            var currentQuestion = $("<h3>").text(this.questionLibrary[this.questionNumber].question).addClass("text-center");
+
+            //Append Game Question to HTML
+            gameContainerTarget.append(currentQuestion);
+
+            //Append Answer Choices
+            for (var i = 0; i < this.questionLibrary[this.questionNumber].choices.length; i++) {
+                var newDiv = $("<h3>");
+                newDiv.text(this.questionLibrary[this.questionNumber].choices[i])
+                //Use Data Class to hold choices index value for comparing to the answer
+                newDiv.addClass("text-center game-choice").attr("data-name", i);
+                gameContainerTarget.append(newDiv);
+            }
+
+
+            $(".game-choice").on("click", function () {
+                console.log("Choice clicked");
+                var playerChoice = $(this).attr("data-name");
+                triviaGame.checkAnswer(playerChoice);
+            });
+
+          
+        },
+
+        //Check If the Selection Is correct and print to screen 
+        checkAnswer: function(param) {
+            console.log("Checking Answer");
+            if (param==this.questionLibrary[this.questionNumber].answer) {
+                console.log("You got it!");
+            } else {
+                console.log("NOPE");
+                
+            }
+
+            //triviaGame.questionNumber++;
+
+        },
+
+
+
+    }
+
+
+    //Game Start Button
+    $("#start-game").on("click", function () {
+        console.log("Game Started");
+        triviaGame.gameStart = true;
+        console.log(triviaGame.gameStart);
+        triviaGame.printQuizToScreen();
+    });
+
+
+
+
+
+});
