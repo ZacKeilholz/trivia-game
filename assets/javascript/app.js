281839722012bd0a58a543f0587968e295e5ac73
Zac Keilholz <keilh001@umn.edu>
2018-04-13 21:32:50 -0500

    Game is at an Excellent spot.  Retiring for the evening.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,4 @@
-/*
 
-1. Game Start screen- background image with main div container that containers a 'start' button and game name
-2. Press start
-3.  On start event, div is cleared (<h1 game name is left alone) and replaced with 
-    a. timer counting down from 30 seconds. 
-    b. Question
-    C. 4 possible answers.  On hover- the individual answer div container background changes to a differnet (complementary) color. 
-4. ANSWER CHOSEN EVENTS 
-    a. WRONG ANSWER CLICKED: Div is cleared, ANSWER PAGE COUNDOWN (5 secs) is started, Div is  replaced with 
-        i. NOPE,  
-        ii. Correct answer was: 
-        iii. Image related to the question.  
-        iv.  After 5 seconds, the div clears and we move to the next question (Go back to #3).
-    b. CORRECT ANSWER CLICKED: Div is cleared, ansser page countdown (5 secs is started), and div is replaced with: 
-        i. YEP
-        ii. Image (or Gif) related to answer. 
-        iii. Countdown ends and we go back to #3.
-5. The END.  When there are no more questions to ask, Div is cleared, and this is displayed: 
-    i All done Here's how you did.
-    ii. Correct Answers
-    iii incorrect answers
-    iv unanswered: 1
-    V.  Start Over Button? 
-6. Start over button Resets the game...  
-
-
-
-    //Create HTML Element
-  /* 
-        var a = $("<button>");
-        a.attr("data-name",movies[i]);
-        a.text(movies[i]);
-        $("#movies-view").append(a);
-        or to append, you could
-        $("#movies-view").append("<button data-name='moviename '>"+movies[i]+"</button>");
-        */
-
-/* 
-Awesome JS Snippets- 
-cl - console log
-si - set interval
-st - set timeout
-jqclass - jq class
-jqempty
-jqdoc - document ready
-*/
 
 $(document).ready(function () {
 
@@ -314,3 +268,50 @@ $(document).ready(function () {
 
 });
 
+/*
+
+1. Game Start screen- background image with main div container that containers a 'start' button and game name
+2. Press start
+3.  On start event, div is cleared (<h1 game name is left alone) and replaced with 
+    a. timer counting down from 30 seconds. 
+    b. Question
+    C. 4 possible answers.  On hover- the individual answer div container background changes to a differnet (complementary) color. 
+4. ANSWER CHOSEN EVENTS 
+    a. WRONG ANSWER CLICKED: Div is cleared, ANSWER PAGE COUNDOWN (5 secs) is started, Div is  replaced with 
+        i. NOPE,  
+        ii. Correct answer was: 
+        iii. Image related to the question.  
+        iv.  After 5 seconds, the div clears and we move to the next question (Go back to #3).
+    b. CORRECT ANSWER CLICKED: Div is cleared, ansser page countdown (5 secs is started), and div is replaced with: 
+        i. YEP
+        ii. Image (or Gif) related to answer. 
+        iii. Countdown ends and we go back to #3.
+5. The END.  When there are no more questions to ask, Div is cleared, and this is displayed: 
+    i All done Here's how you did.
+    ii. Correct Answers
+    iii incorrect answers
+    iv unanswered: 1
+    V.  Start Over Button? 
+6. Start over button Resets the game...  
+
+
+
+    //Create HTML Element
+  /* 
+        var a = $("<button>");
+        a.attr("data-name",movies[i]);
+        a.text(movies[i]);
+        $("#movies-view").append(a);
+        or to append, you could
+        $("#movies-view").append("<button data-name='moviename '>"+movies[i]+"</button>");
+        */
+
+/* 
+Awesome JS Snippets- 
+cl - console log
+si - set interval
+st - set timeout
+jqclass - jq class
+jqempty
+jqdoc - document ready
+*/
